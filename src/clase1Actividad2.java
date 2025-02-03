1154fde4c0d8cd3099a8b02f84f7437cfa787212
rodrosn0w <rodronieva1@gmail.com>
2025-02-03 01:31:21 -0300

    probando

---
+++ src/clase1Actividad2.java
@@ -0,0 +1,17 @@
+public class clase1Actividad2 {
+    public static void main(String[] args) {
+        int edad = 26;
+        double altura = 1.72;
+        char inicial = 'r';
+        String ciudad = "La Matanza";
+
+        int sumaEdad = edad + 20;
+        double alturaDoble = altura * 2;
+        String frase = "Mi inicial es " + inicial + " porque me llamo Rodrigo";
+
+        System.out.println(sumaEdad);
+        System.out.println(alturaDoble);
+        System.out.println(frase);
+
+    }
+}

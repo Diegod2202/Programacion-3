da3aca723ac5bdf5a35c4d0fd265753cbc67aaa5
Diego <diegodangona@gmail.com>
2025-02-09 22:04:32 -0300

    ultima actividad lista

---
+++ src/Clase_7/Actividad_4/Main.java
@@ -0,0 +1,30 @@
+package Clase_7.Actividad_4;
+
+public class Main {
+
+    public static int calcularGananciaMaxima(int[] costos, int[] ganancias, int presupuesto) {
+        int n = costos.length;
+        int[][] dp = new int[n + 1][presupuesto + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= presupuesto; j++) {
+                if (costos[i - 1] <= j) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - costos[i - 1]] + ganancias[i - 1]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][presupuesto];
+    }
+
+    public static void main(String[] args) {
+        int[] costos = {12, 20, 15, 25};
+        int[] ganancias = {150, 200, 100, 300};
+        int presupuesto = 35;
+
+        int gananciaMaxima = calcularGananciaMaxima(costos, ganancias, presupuesto);
+        System.out.println("Ganancia máxima posible: " + gananciaMaxima);
+    }
+}

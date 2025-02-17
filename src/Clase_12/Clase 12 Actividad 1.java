85766366d1eb37637ce3e6269a6f389fcf839c73
Diego <diegodangona@gmail.com>
2025-02-17 00:01:11 -0300

    actividades clase 12

---
+++ src/Clase_12/Clase 12 Actividad 1.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class DFS {
+    private int V; // Número de vértices
+    private LinkedList<Integer> adj[]; // Lista de adyacencia
+
+    DFS(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i)
+            adj[i] = new LinkedList();
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].add(w); // Añadir w a la lista de v
+    }
+
+    void DFSUtil(int v, boolean visited[]) {
+        visited[v] = true;
+        System.out.print(v + " ");
+
+        for (int n : adj[v]) {
+            if (!visited[n])
+                DFSUtil(n, visited);
+        }
+    }
+
+    void DFS(int v) {
+        boolean visited[] = new boolean[V];
+        DFSUtil(v, visited);
+    }
+
+    public static void main(String args[]) {
+        DFS g = new DFS(9);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(1, 4);
+        g.addEdge(2, 5);
+        g.addEdge(2, 6);
+        g.addEdge(3, 7);
+        g.addEdge(4, 8);
+
+        System.out.println("Recorrido DFS comenzando desde el nodo 0:");
+        g.DFS(0);
+    }
+}
